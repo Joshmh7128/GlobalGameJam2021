89b5c9d770521e5437f369cc1cc4e1dcba7a264a
Robert Lindquist <robert.lindquist98@gmail.com>
2021-01-29 21:40:36 -0500

    Made player input system
    - Takes rewired input into PlayerInputComponent, stores in public
    properties for ship movement system to use.

---
+++ Assets/Scripts/RobDev/Ship Movement/PlayerInputComponent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerInputComponent : InputComponent
+{
+    /// <summary>
+    /// The Rewired Player this input component uses to get input from the player
+    /// </summary>
+    private Player inputPlayer;
+
+    void Start()
+    {
+        inputPlayer = ReInput.players.GetSystemPlayer();
+
+        Debug.Log(inputPlayer.descriptiveName);
+    }
+
+    private void Update()
+    {
+        GetPlayerInput();
+    }
+
+    private void GetPlayerInput()
+    {
+        if (inputPlayer != null)
+        {
+            Pause = inputPlayer.GetButtonDown("Pause");
+
+            Forward = inputPlayer.GetButtonDown("Forwards");
+
+            Backward = inputPlayer.GetButtonDown("Backwards");
+
+            RudderLeft = inputPlayer.GetButtonDown("RudderLeft");
+
+            RudderRight = inputPlayer.GetButtonDown("RudderRight");
+
+            SailsLeft = inputPlayer.GetButtonDown("SailsLeft");
+
+            SailsRight = inputPlayer.GetButtonDown("SailsRight");
+        }
+    }
+}
