f8320c0492ad837237b1295b1c5271f469212bf6
Josh <joshmh7128@gmail.com>
2021-01-30 13:50:08 -0500

    Fixed Ocean, Added Tile Gen

---
+++ Assets/Scripts/JoshDev/TileVariationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileVariationScript : MonoBehaviour
+{
+    /// 
+    /// script works by enabling one of many child objects on the tile
+    /// will also rotate tile at 90 degree intervals for randomness
+    /// 
+
+    private void Start()
+    {
+        // rotate randomly
+        int i = Random.Range(0, 3); // for rotation
+        transform.Rotate(0, 90 * i, 0); // perform rotation
+
+        // enable a random object
+        transform.GetChild(Random.Range(0, transform.childCount)).gameObject.SetActive(true);
+    }
+}
