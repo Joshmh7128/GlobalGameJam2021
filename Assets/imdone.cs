aa2e4ee433edc6eec68b423eef5cce2713dd1178
Unknown <justinkevinwoods@gmail.com>
2021-01-31 10:46:52 -0500

    added small boat and tuna

---
+++ Assets/imdone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imdone : MonoBehaviour
+{
+    public Transform emppty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        emppty = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
